f97cddd9c681c3c6b5bfe2ffdf22ae427673fdcf
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-20 16:54:46 +0530

    add about update

--- src/Component/About/TeamSection.js
+++ src/Component/About/TeamSection.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
-import Team1 from "../../assets/images/team.jpeg";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+// import Team1 from '../../assets/images/team.jpeg';
+import axios from 'axios';
 
 export default function TeamSection() {
+ 
   const [drTeam, setDrTeam] = useState([]);
 
   useEffect(() => {
     // Define the backend API endpoint URL
-    const apiUrl = "https://eikon-api.onrender.com/drList/";
+    const apiUrl = 'https://eikon-api.onrender.com/drList/';
 
     // Make an HTTP GET request to the backend
     axios
@@ -18,7 +19,7 @@ export default function TeamSection() {
         setDrTeam(...data);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error);
       });
   }, []);
 
@@ -26,24 +27,22 @@ export default function TeamSection() {
   let teamListItems = null;
   if (drTeam.DrTeamList) {
     teamListItems = drTeam.DrTeamList.map((drTeam) => (
-      <div className="col-lg-4 col-md-4" key={drTeam._id}>
-        <div className="professional-box-item text-center">
-          <a>
-            <figure className="mb-0">
-              <img
-                src={drTeam.image}
-                alt="professional-box-img"
-                className="img-fluid rounded-circle"
-              />
-            </figure>
-            <h5>{drTeam.Name}</h5>
-            <small className="d-block">{drTeam.Post}</small>
-            <span className="d-block">{drTeam.description}</span>
-          </a>
-          <div className="professional-box-social-list">
-            <a href="team-detail.html"></a>
-          </div>
-        </div>
+      <div className="col-lg-4 col-md-4" key={drTeam.id}>
+      
+                                    <div className="professional-box-item text-center">
+                                        <a href="team-detail.html">
+                                            <figure className="mb-0">
+                                                <img src={drTeam.image} alt="professional-box-img" className="img-fluid rounded-circle" />
+                                            </figure>
+                                            <h5>{drTeam.Name}</h5>
+                                            <small className="d-block">{drTeam.Post}</small>
+                                            <span className="d-block">{drTeam.description}</span> 
+                                        </a><div className="professional-box-social-list"><a href="team-detail.html">
+                                        </a>
+                                     
+                                        </div>
+
+                                    </div>
       </div>
     ));
   }
